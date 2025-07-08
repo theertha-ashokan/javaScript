7d4374a8d8b7d71b12f4afbe5645e4d5aa624618
theertha-ashokan <theerthauk13@gmail.com>
2025-07-08 20:12:15 +0530

    nesteed array

---
+++ nestedarr.js
@@ -0,0 +1,42 @@
+a = [[5,6],[11,9],[3,10]]
+console.log(a);
+console.log('--------------------------');
+
+//w.a.p to print all numabers from the given array
+
+for(let item of a){
+    for(let num of item)
+    {
+        console.log(num);
+        
+    }
+}
+console.log('---------------------');
+
+//w.a p.to print sum of all numbers from the given array
+a = [[5,6],[11,9],[3,10]]
+sum = 0
+for(let subarray of a){
+    for(let num of subarray){
+        sum+=num
+    }
+}
+console.log(sum);
+console.log('---------------------------');
+
+//
+
+b = [[[5,6]],[[10,0]],[[11,9]],[[55,60]],[[3,10]],[]]
+
+
+
+
+
+
+
+
+
+
+
+
+
