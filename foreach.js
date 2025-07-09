7c5c6f098c84f5d4bcae299fd1813087f2b87b89
theertha-ashokan <theerthauk13@gmail.com>
2025-07-09 13:02:54 +0530

    array op

---
+++ foreach.js
@@ -0,0 +1,15 @@
+a = [10,11,12,13,14,15]
+//to print square of each number in a given array
+for(let item of a){
+    console.log(item**2);
+    
+}
+console.log('------------------');
+
+a.forEach((item)=>console.log(item**2));
+
+console.log('------------------');
+
+
+
+
