2518598e64df77b849b787c51eadf82e41c685d2
theertha-ashokan <theerthauk13@gmail.com>
2025-07-09 13:04:16 +0530

    array op

---
+++ filter.js
@@ -0,0 +1,16 @@
+//w.ap.to display all even numbers from the given array
+//normal way using array of
+a = [10,11,12,13,14,15]
+
+  for(let item of a){
+    if(item%2==0){
+        console.log(item);
+        
+    }
+  }
+  console.log('----------------------');
+  
+  //using filter
+  even = a.filter((item)=>item%2==0)
+  console.log(even);
+  
