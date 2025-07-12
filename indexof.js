a34697216a813ec21b91cb488a23dd515cc4ded4
theertha-ashokan <theerthauk13@gmail.com>
2025-07-12 16:31:11 +0530

    indexof

---
+++ indexof.js
@@ -0,0 +1,34 @@
+
+a = ['manu','mini','max']
+x = a.indexOf('mini')
+console.log(x);
+//return the index valu
+
+console.log('--------------------------');
+
+
+//lastindexOf
+b=[1,2,3,4,5,6,2]
+
+d = b.indexOf(2)
+console.log(d);
+console.log('-------------------------------');
+
+
+e = b.lastIndexOf(2)
+console.log(e);
+console.log('--------------------------------');
+
+
+//slice
+//slice(startindex,end index)
+f=b.slice(2,6)
+console.log(f);
+console.log('--------------------------------');
+
+
+//if we give only one index...arry print after that index
+g=b.slice(5)
+console.log(g);
+console.log('--------------------------------');
+
